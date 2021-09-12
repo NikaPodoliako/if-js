b368a4524087fb12bff051ae22cfbe52a17c0d02
NikaPodoliako <qrmiss.nl@gmail.com>
2021-09-12 18:12:41 +0300

    Changed variables from let to const

--- main.js
+++ main.js
@@ -14,21 +14,21 @@ console.log(test);
 test = true; // true
 console.log(test);
 
-let arr = [2, 3, 5, 8];
+const arr = [2, 3, 5, 8];
 let res = 1;
 for (let i = 0; i < arr.length; i++) {
 	res *= arr[i] 
 }
 console.log(res);
 
-let arr2 = [2, 5, 8, 15, 0, 6, 20, 3];
+const arr2 = [2, 5, 8, 15, 0, 6, 20, 3];
 	for (let i = 0; i < arr2.length; i++) {
 		if (arr2[i] > 5 && arr2[i] < 10) { 
 			console.log(arr2[i]);
 			}
 	}
 
-let arr3 = [2, 5, 8, 15, 0, 6, 20, 3];
+const arr3 = [2, 5, 8, 15, 0, 6, 20, 3];
 	for (let i = 0; i < arr3.length; i++) {
 		if (arr3[i] % 2 === 0) {
 			console.log(arr3[i]);
