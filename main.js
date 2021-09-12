efad1fc7a1f52e5daabff1580483456b1cbdb22d
NikaPodoliako <qrmiss.nl@gmail.com>
2021-09-13 00:23:06 +0300

    Created a function to check a word for a palidrome, found min and max values ​​of numbers

--- main.js
+++ main.js
@@ -34,3 +34,37 @@ const arr3 = [2, 5, 8, 15, 0, 6, 20, 3];
 			console.log(arr3[i]);
 		}
 	}	
+
+	//lesson-3
+
+let palindromeWord = 'kok';
+const newPal = palindromeWord.split('').reverse().join('');
+function palidrome() {
+	(newPal === palindromeWord) ? console.log(true) : console.log(false); 
+}
+palidrome();
+
+
+function min(a, b) {
+		if (a < b) {
+			console.log('Min number', a);
+	} 	else {
+			console.log('Min number', b);
+	}
+}
+min(9, 8);
+
+function max(a, y) {
+	(a > y) ? console.log('Max number', a) : console.log('max number', y)
+}
+max(5, 2);
+
+let arr = [58, 96, 12, 73, 28, 35, 5, 88, 0, 41, 100]; // данное дз показала Марта, сама сделать его не смогла, до сих немного не понимаю его
+function changeNumber(a) {
+	const num = a.join('-');
+	if (num.includes('0')){
+		return num.replaceAll('0', 'zero');
+	}
+	return num;
+}
+console.log([arr].map(changeNumber)); 
